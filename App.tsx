f01ecb8dbaf91bd46d5728d683ef2102be6268d3
Neo <neo@polarislab.xyz>
2026-02-19 12:38:42 +0100

    fix: disable react-freeze to fix type mismatch error

--- App.tsx
+++ App.tsx
@@ -1,6 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { AppNavigator } from './src/navigation';
+import { enableFreeze } from 'react-native-screens';
+
+// Disable react-freeze to avoid type mismatch in new architecture
+enableFreeze(false);
 
 export default function App() {
   return (
